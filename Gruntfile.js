5782d10f3af06ad4a1763b98a0a84c911f272403
Matthew Lewis <matt@mplewis.com>
2014-01-29 01:27:39 -0600

    Replace PyPI API with simple load via copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,59 +3,48 @@ var fs = require('fs');
 var path = require('path');
 var rmdir = require('rimraf');
 var request = require('request');
+var ncp = require('ncp').ncp;
+var async = require('async');
+
+ncp.limit = 16;
 
 var DEFAULT_PYEXEC = '#!/usr/bin/env python';
-var DEFAULT_DIST = 'dist/';
+var DEFAULT_DIST = 'dist';
 
-var LIB_DIR = 'src/coralcgi/libs';
-
-var PYPI_PKG = 'http://pypi.python.org/pypi/%s/json';
-var PYPI_PKG_VER = 'http://pypi.python.org/pypi/%s/%s/json';
-
-var pypiApi = {
-  info: function(pkg, ver, callback) {
-    if (!ver) {   
-      var url = util.format(PYPI_PKG, pkg);
-      request(url, function(err, resp, body) {
-        if (err)
-          return callback(err);
-        if (resp.statusCode !== 200)
-          return callback(new Error(body));
-        var info = JSON.parse(body);
-        return callback(err, info);
-      });
-    }
-  }
-};
+var LIB_SRC_DIR = 'python_libs';
+var LIB_DIST_DIR = 'src/coralcgi/libs';
 
 module.exports = function(grunt) {
 
   var pypiHandlers = {
-    load: function(pkg, ver, done) {
-      var text;
-      if (ver)
-        text = util.format('Loading package %s, version %s', pkg, ver);
-      else
-        text = util.format('Loading package %s', pkg);
-      grunt.log.writeln(text);
-      pypiApi.info(pkg, ver, function(err, info) {
-        if (err) {
-          grunt.log.error('Error: ' + err.message);
-          grunt.fail.warn('Loading package failed.');
-          done();
-        } else {
-          grunt.log.writeln(util.format('%s info:', pkg));
-          grunt.log.writeln(util.format(' - Version:', info.info.version));
-          done();
-        }
-      });
+    load: function(pkg, done) {
+      grunt.log.writeln(util.format('Loading package %s', pkg));
+      var srcPath = path.join(LIB_SRC_DIR, pkg);
+      var distPath = path.join(LIB_DIST_DIR, pkg);
+      if (fs.existsSync(distPath)) {
+        grunt.log.error(util.format('ERROR: %s already exists in %s'), pkg, LIB_DIST_DIR);
+        grunt.fail.warn('Loading package failed.');
+      } else if (fs.existsSync(srcPath) && fs.statSync(srcPath).isDirectory()) {
+        ncp(srcPath, distPath, function(err) {
+          if (err) {
+            grunt.log.error(util.format('ERROR: %s'), err.message);
+            grunt.fail.warn('Loading package failed.');
+          } else {
+            grunt.log.ok();
+            done();
+          }
+        });
+      } else {
+        grunt.log.error(util.format('Package %s not found in %s.', pkg, LIB_SRC_DIR));
+        grunt.fail.warn('Loading package failed.');
+      }
     },
     unload: function(pkg) {
       var text = util.format('Unloading package %s', pkg);
       grunt.log.writeln(text);
-      var pkgpath = path.join(LIB_DIR, pkg);
-      if (fs.existsSync(pkgpath) && fs.statSync(pkgpath).isDirectory()) {
-        rmdir.sync(pkgpath);
+      var pkgPath = path.join(LIB_DIST_DIR, pkg);
+      if (fs.existsSync(pkgPath) && fs.statSync(pkgPath).isDirectory()) {
+        rmdir.sync(pkgPath);
         grunt.log.ok();
       } else {
         var pkgtext = util.format('%s not loaded. View loaded packages ' + 
@@ -65,10 +54,9 @@ module.exports = function(grunt) {
       }
     },
     list: function(pkg) {
-      var files = fs.readdirSync(LIB_DIR);
+      var files = fs.readdirSync(LIB_DIST_DIR);
       files = files.map(function(file) {
-        return {'name': file,
-                'path': path.join(LIB_DIR, file)};
+        return {'name': file, 'path': path.join(LIB_DIST_DIR, file)};
       }).filter(function(obj) {
         return fs.statSync(obj.path).isDirectory();
       });
@@ -126,10 +114,20 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('pypi', function(task, pkg, ver) {
+  grunt.registerTask('pypi', function(task, pkg) {
     if (task === 'load') {
-      var done = this.async();
-      pypiHandlers.load(pkg, ver, done);
+      var allDone = this.async();
+      async.series([
+        function(done) {
+          pypiHandlers.load(pkg, done);
+        },
+        function(done) {
+          pypiHandlers.list();
+          done();
+        }
+      ], function() {
+        allDone();
+      });
     } else if (task === 'unload') {
       pypiHandlers.unload(pkg);
       pypiHandlers.list();
