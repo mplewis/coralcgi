2b32a467ffbbd269c75b5019b3786962e812dec6
Matthew Lewis <matt@mplewis.com>
2014-01-29 03:09:13 -0600

    add relative_dir support to CoralCGI.setup()

--- src/coralcgi/__init__.py
+++ src/coralcgi/__init__.py
@@ -1,13 +1,16 @@
 import cgidebug
 from headers import ContentType
 
-# Setup library paths
 import sys
-sys.path.append('coralcgi/libs')
+import os
+
+LIB_DIR = 'coralcgi/libs'
 
 
-def setup(debug=False, html=True):
+def setup(debug=False, html=True, relative_dir=''):
     if debug:
         cgidebug.enable()
     if html:
         ContentType.html()
+    # Setup library paths
+    sys.path.append(os.path.join(relative_dir, LIB_DIR))
