40e15503725fc2373d654fa2904740fdea911e74
Matthew Lewis <matt@mplewis.com>
2014-01-31 13:24:43 -0600

    change relative_dir to relative_path

--- src/coralcgi/__init__.py
+++ src/coralcgi/__init__.py
@@ -7,10 +7,10 @@ import os
 LIB_DIR = 'coralcgi/libs'
 
 
-def setup(debug=False, html=True, relative_dir=''):
+def setup(debug=False, html=True, relative_path=''):
     if debug:
         cgidebug.enable()
     if html:
         ContentType.html()
     # Setup library paths
-    sys.path.append(os.path.join(relative_dir, LIB_DIR))
+    sys.path.append(os.path.join(relative_path, LIB_DIR))
