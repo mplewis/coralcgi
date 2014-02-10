6bf0ff77661d4a6bdcfc30ee423e763c5531ee04
Matthew Lewis <matt@mplewis.com>
2014-02-10 16:00:25 -0600

    Group tests

--- test/test_coralcgi.py
+++ test/test_coralcgi.py
@@ -15,32 +15,32 @@ from coralcgi import request
 import os
 
 
-@patch('coralcgi.cgidebug.enable')
-@patch('coralcgi.headers.ContentType.html')
-@patch('sys.path')
-def test_setup(mock_path, mock_html, mock_enable):
-    TEST_REL_PATH = 'rel/path/test'
-    appended_path = os.path.join(TEST_REL_PATH, coralcgi.LIB_DIR)
+class TestCoralCGI:
+    @patch('coralcgi.cgidebug.enable')
+    @patch('coralcgi.headers.ContentType.html')
+    @patch('sys.path')
+    def test_setup(self, mock_path, mock_html, mock_enable):
+        TEST_REL_PATH = 'rel/path/test'
+        appended_path = os.path.join(TEST_REL_PATH, coralcgi.LIB_DIR)
+        coralcgi.setup(debug=True, html=False, relative_path=TEST_REL_PATH)
+        mock_enable.assert_called()
+        mock_html.assert_not_called()
+        mock_path.append.assert_called_with(appended_path)
 
-    coralcgi.setup(debug=True, html=False, relative_path=TEST_REL_PATH)
 
-    mock_enable.assert_called()
-    mock_html.assert_not_called()
-    mock_path.append.assert_called_with(appended_path)
-
-
-@patch('cgitb.enable')
-def test_cgidebug(mock_cgitb_enable):
-    cgidebug.enable('test_arg', test_kwarg='test_val')
-    mock_cgitb_enable.assert_called_with('test_arg', test_kwarg='test_val')
+class TestCGIDebug:
+    @patch('cgitb.enable')
+    def test_cgidebug(self, mock_cgitb_enable):
+        cgidebug.enable('test_arg', test_kwarg='test_val')
+        mock_cgitb_enable.assert_called_with('test_arg', test_kwarg='test_val')
 
 
 class TestRequest:
-    def test_request_method(mock_environ):
+    def test_request_method(self):
         with patch.dict('os.environ', {'REQUEST_METHOD': 'GET'}):
             request.method().should.equal('GET')
 
-    def test_request_query_raw(mock_environ):
+    def test_request_query_raw(self):
         query_string = 'thing1=one&thing2=two'
         with patch.dict('os.environ', {'QUERY_STRING': query_string}):
             request.query_raw().should.equal(query_string)
