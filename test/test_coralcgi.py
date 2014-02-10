25a01ae4a5a2f83305385ddcd0c09b6534746c09
Matthew Lewis <matt@mplewis.com>
2014-02-10 15:18:45 -0600

    Improve mocks to remove side effects; improve docs

--- test/test_coralcgi.py
+++ test/test_coralcgi.py
@@ -1,23 +1,34 @@
+# Append to path so we can find/import CoralCGI
 import sys
 sys.path.append('src')
 
-import coralcgi
-
-import os
+# Test framework modules
 import sure  # noqa
-from mock import MagicMock
+from mock import patch
+
+# Modules to test
+import coralcgi
+from coralcgi import cgidebug
+
+# Modules mocked for testing
+import os
 
 
-def test_setup():
+@patch('coralcgi.cgidebug.enable')
+@patch('coralcgi.headers.ContentType.html')
+@patch('sys.path')
+def test_setup(mock_path, mock_html, mock_enable):
     TEST_REL_PATH = 'rel/path/test'
     appended_path = os.path.join(TEST_REL_PATH, coralcgi.LIB_DIR)
 
-    coralcgi.cgidebug.enable = MagicMock()
-    coralcgi.headers.ContentType.html = MagicMock()
-    sys.path = MagicMock()
-
     coralcgi.setup(debug=True, html=False, relative_path=TEST_REL_PATH)
 
-    coralcgi.cgidebug.enable.assert_called()
-    coralcgi.headers.ContentType.html.assert_not_called()
-    sys.path.append.assert_called_with(appended_path)
+    mock_enable.assert_called()
+    mock_html.assert_not_called()
+    mock_path.append.assert_called_with(appended_path)
+
+
+@patch('cgitb.enable')
+def test_cgidebug(mock_cgitb_enable):
+    cgidebug.enable('test_arg', test_kwarg='test_val')
+    mock_cgitb_enable.assert_called_with('test_arg', test_kwarg='test_val')
